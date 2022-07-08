2f587fc027ad63431803c449055c8e221d0e1eb9
RinkM <mikerinkert@gmail.com>
2022-07-08 14:28:36 -0600

    from Mac. Added some button functions for testing.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let var1;
+let var1 = "";
 let var2;
 let var3;
 let answer;
@@ -6,46 +6,92 @@ let answer;
 let num1 = 15;
 let num2 = 3;
 let result;
+let var1Status = true;
 
 let allButtons = document.querySelectorAll(".button");
-
-let btnAC = document.querySelector(".btnAC");
-let btnNegative = document.querySelector(".btnNegative");
-let btnPercent = document.querySelector(".btnPercent");
-let btnDivide = document.querySelector(".btnDivide");
 
 
 // const daButtons = allButtons.map(btn => {
 //     addEventListener(onclick, console.log("click") )
 // })
 
-
-const btnSeven = document.querySelector(".btnSeven");
-const btnEight = document.querySelector(".btnEight");
-const btnNine = document.querySelector(".btnNine");
-const btnMultiply = document.querySelector(".btnMultiply");
-
-const btnFive = document.querySelector(".btnFive");
-const btnSix = document.querySelector(".btnSix");
-const btnFour = document.querySelector(".btnFour");
-const btnMinus = document.querySelector(".btnMinus");
-
 const btnOne = document.querySelector(".btnOne");
 const btnTwo = document.querySelector(".btnTwo");
 const btnThree = document.querySelector(".btnThree");
+const btnFour = document.querySelector(".btnFour");
+const btnFive = document.querySelector(".btnFive");
+const btnSix = document.querySelector(".btnSix");
+const btnSeven = document.querySelector(".btnSeven");
+const btnEight = document.querySelector(".btnEight");
+const btnNine = document.querySelector(".btnNine");
+const btnZero = document.querySelector(".btnZero");
+
+const btnDivide = document.querySelector(".btnDivide");
+const btnMultiply = document.querySelector(".btnMultiply");
+const btnMinus = document.querySelector(".btnMinus");
 const btnPlus = document.querySelector(".btnPlus")
 
-const btnZero = document.querySelector(".btnZero");
 const btnDecimal = document.querySelector(".btnDecimal");
 const btnEqual = document.querySelector(".btnEqual");
+
+const btnAC = document.querySelector(".btnAC");
+const btnNegative = document.querySelector(".btnNegative");
+const btnPercent = document.querySelector(".btnPercent");
 
 const screen  = document.querySelector(".screen");
 
 
+btnAC.addEventListener('click', ()=>{
+    buttonClear();});
+btnOne.addEventListener('click', ()=>{
+    buttonNumber(1);});
+btnTwo.addEventListener('click', ()=>{
+    buttonNumber(2);});
+btnThree.addEventListener('click', ()=>{
+    buttonNumber(3);});
+btnPlus.addEventListener('click', ()=> {
+    buttonOperation("add");});
 
 
-function buttonPress(buttonName){
+// operator function turns on or off var 1 or var2.  
+// var1status = false
+// var2status = true
+// if var1Status = 
 
+
+// btnOne.addEventListener(c, buttonPress(1) )
+// btnTwo.addEventListener(onclick, buttonPress(2) )
+
+function buttonClear(){
+    var1Status = true
+    var1 = ""
+    screen.textContent = var1
+}
+
+function buttonOperation(operation) {
+    let symbol = ""
+    if (operation == "add"){
+        symbol = "+";
+    }else if(operation == "sub"){
+        symbol = "-";
+    }else if(operation == "times"){
+        symbol = "*";
+    }else if(operation == "divide"){
+        symbol = "/";
+    }else {console.log("Error : buttonOperation didn't operate.")
+    }
+    var1 = var1 + symbol;
+    screen.textContent = var1
+    var1Status = false;
+}
+
+
+function buttonNumber(number){
+   let string = number.toString()
+   
+    var1 = var1 + string;
+    console.log(var1)
+    screen.textContent = var1
 
 }
 
--- notes.md
+++ notes.md
@@ -1,4 +1,26 @@
 
+issues : 
+able to print +++++++ in screen.
+
+
+
+
+
+how to :
+screen is only for display.  not for the actual behind the scenes part.
+
+on number press, add to display.  which is separate to add to variable  1,11,113 etc.
+how do I know if it's var1 or var2?  
+on operator press... add to display.  save selected operator.  'add' 'sub', etc.
+flip to var2
+
+equal
+
+button1 - edits var1
+buttonOperator - chooses the operator and opens var2.   how can there only be one
+ +- etc. on screen?
+ Button2 edits var2
+
 
 List of all the buttons CSS class names :
 
